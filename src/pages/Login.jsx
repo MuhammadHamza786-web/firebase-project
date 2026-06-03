222ce112cb14fc43e5bd18c68d6b4b24dfd0511d
MuhammadHamza786-web <hamzasiraj.470@gmail.com>
2026-06-03 10:51:24 +0500

    Add code

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,8 +4,16 @@ import { Link } from 'react-router-dom'
 const login = () => {
   return (
     <div className='login'>
+     <form action="">
 
     <div>login page</div>
+    <input type="text" placeholder='Enter your Email' />
+    <br />
+    <input type="password" placeholder='Enter your password'/>
+    <br />
+    <button>Sign In</button>
+
+     </form>
     
     </div>
   )
--- src/pages/Sigin.jsx
+++ src/pages/Sigin.jsx
@@ -6,7 +6,14 @@ const sigin = () => {
     <div className='sigin'>
 
     <div>sigin page</div>
-    
+    <form action="">
+
+    <input type="text" placeholder='Enter your Email' />
+    <br />
+    <input type="password" placeholder='Enter your password'/>
+    <br />
+    <button>Sign In</button>
+    </form>
     </div>
   )
 }
