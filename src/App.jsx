96de99578e6f0175422cb0ab78bc9d771e84139e
MuhammadHamza786-web <hamzasiraj.470@gmail.com>
2026-06-05 10:20:22 +0500

    Add code

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,19 @@
 import './App.css';
 import Sigin from "./pages/Sigin";
 import Login from "./pages/Login";
-import Home from  "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Header from './pages/Header';
 import { Routes , Route } from 'react-router-dom';
 
 function App() {
   
   return (
     <div className="App">
-      <Header/>
-      <header className='header'>
+      
 
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/About" element={<About/>}/>  
-        <Route path="/Contact" element={<Contact/>}/>  
         <Route path="/Sigin" element={<Sigin/>}/>
         <Route path="/Login" element={<Login/>}/>  
         
       </Routes>
-      </header>
     </div>
   );
 }
--- src/pages/Sigin.jsx
+++ src/pages/Sigin.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const sigin = () => {
+  
+<Link to={"/Login"}>login</Link>
+
   return (
+    
     <div className='sigin'>
 
     <div>sigin page</div>
