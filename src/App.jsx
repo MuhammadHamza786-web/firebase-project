c69a491e31dae09cc669ed231edb13d9a1abc5d7
MuhammadHamza786-web <hamzasiraj.470@gmail.com>
2026-06-05 11:52:04 +0500

    Add code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css';
 import Sigin from "./pages/Sigin";
 import Login from "./pages/Login";
-import { Routes , Route } from 'react-router-dom';
+import dashboard from "./pages/dashboard";
+import {Navigate , Routes , Route } from 'react-router-dom';
+import { useEffect , useState } from 'react';
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
   
@@ -10,8 +14,11 @@ function App() {
       
 
       <Routes>
+        <Route path="/" element={<dashboard/>}/>
         <Route path="/Sigin" element={<Sigin/>}/>
-        <Route path="/Login" element={<Login/>}/>  
+        <Route path="/Login" element={<Login/>}/>
+        <Route path="*" element={<Navigate to={"/"}/>}/>
+
         
       </Routes>
     </div>
--- src/pages/Sigin.jsx
+++ src/pages/Sigin.jsx
@@ -1,25 +1,45 @@
 import React from 'react'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
-const sigin = () => {
+
+const Sigin = () => {
+  const auth = getAuth();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const SigninUser = (e)=>{  
+  e.preventDefault()
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    console.log("userCredential", userCredential)
+  })
+  .catch((error) => {
+    console.log("eror", error)
+  });
   
-<Link to={"/Login"}>login</Link>
-
+}
   return (
-    
-    <div className='sigin'>
+    <div className='signin'>
 
-    <div>sigin page</div>
-    <form action="">
-
-    <input type="text" placeholder='Enter your Email' />
-    <br />
-    <input type="password" placeholder='Enter your password'/>
-    <br />
-    <button>Sign In</button>
-    </form>
+      <h1>Sign In</h1>
+      <form action="">
+        <label htmlFor="">
+          Email:{""}
+          <input type="email" placeholder='Enter Your Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Password:{""}
+          <input type="password" placeholder='Enter Your Password'  value={password} onChange={(e)=>setPassword(e.target.value)}/>
+        </label>
+        <br />
+        <button>Sign In</button>
+      </form>
     </div>
   )
 }
 
-export default sigin
+export default Sigin
